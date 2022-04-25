44d03ec8a096c8922c7ccecb726ee9427f8bf301
joseph <joseph@DESKTOP-CKQG0VO.localdomain>
2022-04-25 12:39:24 +0200

    admin ui grid not loading, no error

--- Controller/Adminhtml/Store/Index.php
+++ Controller/Adminhtml/Store/Index.php
@@ -6,56 +6,26 @@
 
 namespace Joseph\StoreLocator\Controller\Adminhtml\Store;
 
-//todo : add to module sequence
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
-use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\View\Result\Page;
-use Magento\Framework\View\Result\PageFactory;
 
 /**
  * Class Index
  */
-class Index extends Action implements HttpGetActionInterface
+class Index extends Action
 {
-    const MENU_ID = 'Joseph_StoreLocator::Store_Locator';
-
     /**
      * Authorization level of a basic admin session
      */
     const ADMIN_RESOURCE = 'Joseph_StoreLocator::Store_Locator';
 
     /**
-     * @var PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
-     * Index constructor.
+     * Index action
      *
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
+     * @return void
      */
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        parent::__construct($context);
-
-        $this->resultPageFactory = $resultPageFactory;
-    }
-
-    /**
-     * Load the page defined in view/adminhtml/layout/storelocator_store_index.xml
-     *
-     * @return Page
-     */
-    public function execute(): Page
+    public function execute(): void
     {
-        $resultPage = $this->resultPageFactory->create();
-        $resultPage->setActiveMenu(static::MENU_ID);
-        $resultPage->getConfig()->getTitle()->prepend(__('Hello World'));
-
-        return $resultPage;
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
     }
 }
