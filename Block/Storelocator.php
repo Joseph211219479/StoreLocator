0fae1cc890085748d0ffee2b7c541df66e38c917
joseph <joseph@DESKTOP-CKQG0VO.localdomain>
2022-05-06 15:40:49 +0200

    init index.php and storelocator.phtml

---
+++ Block/Storelocator.php
@@ -0,0 +1,30 @@
+<?php
+namespace Joseph\StoreLocator\Block;
+use Magento\Framework\View\Element\Template;
+use Joseph\StoreLocator\Api\StoreRepositoryInterface;
+
+class Storelocator extends \Magento\Framework\View\Element\Template
+{
+    private StoreRepositoryInterface $storeRepository;
+
+    /**
+     * @param Template\Context $context
+     * @param array $data
+     * @param StoreRepositoryInterface $storeRepository
+     */
+    public function __construct(Template\Context $context, array $data = [], StoreRepositoryInterface $storeRepository
+)
+    {
+        parent::__construct($context, $data);
+        $this->storeRepository = $storeRepository;
+    }
+
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+    public function getStorelocatorCollection(){
+        return $this->storeRepository->getListGroupedByProvince();
+    }
+}
